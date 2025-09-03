d1051016d67fc2c80c82b184c957bb78c69dfc84
Tinkle01 <1021287060@qq.com>
2025-09-03 16:54:17 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -66,22 +66,14 @@ def convert_y(y):
 
 
 def convert_z(z):
-    '''
-    将频率转化为音调
-    '''
     z[z > 0] = torch.round(12 * torch.log2(z[z > 0] / 440) + 49 - 2)
     return z.long()
 
 
 
 def freq2tone(freq, noteFrequency):
-    """
-    将频率转换为音调索引
-    """
     indices = torch.zeros(freq.shape)
-
     for i in range(len(noteFrequency)):
-        # 找到频率大于 noteFrequency[i] 的位置
         mask = (freq > noteFrequency[i])
         indices[mask] = i
 
@@ -89,9 +81,7 @@ def freq2tone(freq, noteFrequency):
 
 
 def convert_z2(z):
-    """
-    将频率转化为音调
-    """
+
     standardFrequency = 440.0
     noteFrequency2 = [(standardFrequency / 32.0) * math.pow(2, (i - 9.0) / 12.0) for i in range(128)]
     noteFrequency = []
@@ -99,12 +89,8 @@ def convert_z2(z):
         if i >= 23 and i < 89:
             noteFrequency.append(noteFrequency2[i])
 
-    # 将 noteFrequency 转换为张量
     noteFrequency_tensor = torch.tensor(noteFrequency)
-
-
     z = z.cpu()
-    # 使用 freq2tone 进行转换
     z[z > 0] = freq2tone(z[z > 0], noteFrequency_tensor)
     return z.long()
 
@@ -196,7 +182,7 @@ def load_data(fp, mode='train'):
     z = []
     for line in f.readlines():
         y.append(float(line.strip().split()[1]))
-    num_seg = min(len(y) // LEN_SEG, num_seg)  # 防止X比y长
+    num_seg = min(len(y) 
     y = torch.tensor(np.array([y[LEN_SEG * i:LEN_SEG * i + LEN_SEG] for i in range(num_seg)]), dtype=torch.float32)
     if mode == 'train':
         p = y.clone()
@@ -244,51 +230,36 @@ def ce_weight(data):
 
 def DSL(pred_u_self, eta=0.95,max_class_num=15):
     with torch.no_grad():
-        # 初始化 DSL 结果
+
         dsl_y_star = torch.zeros_like(pred_u_self)
-
-        # 获取所有帧的预测概率
         pt = pred_u_self.permute(2, 0, 1)  # 形状变为 (num_frames, bs, num_classes)
-
-        # 按概率降序排序
         sorted_probs, sorted_indices = torch.sort(pt, descending=True, dim=2)
-        # 计算累计概率，找到阈值类别
         accumulated_probs = torch.cumsum(sorted_probs, dim=2)
         C_star = (accumulated_probs >= eta).int().argmax(dim=2, keepdim=True)
         delta_j = sorted_probs.gather(2, C_star)
-        # 只取前15个主导类别的概率
         sorted_probs, sorted_indices = (
             sorted_probs[:, :, :max_class_num],
             sorted_indices[:, :, :max_class_num],
         )
         dominant_classes = sorted_indices * (sorted_probs >= delta_j).int()
-        # 创建主导类别掩码
         h_j = torch.zeros_like(pt)
         h_j.scatter_(2, dominant_classes, 1)
-
-        # 计算动态软标签 (DSL),hj说明哪些类别是主导类别
-        # 只取主导类别的概率
         y_star = h_j * pt
         y_star /= torch.sum(y_star, dim=2, keepdim=True) + 1e-10
-
-        # 保存结果
-        dsl_y_star = y_star.permute(1, 2, 0)  # 形状变回 (bs, num_classes, num_frames)
+        dsl_y_star = y_star.permute(1, 2, 0)
 
         return dsl_y_star
 
 
 def calculate_cnr_factor(pred_u_self, pred_u_ema, global_confidence, m,max_class_num=15):
     #with torch.no_grad():
-        # 获取所有帧的预测概率
         pt = pred_u_self.permute(2, 0, 1)  # 形状变为 (num_frames, bs, num_classes)
         pt_ema = pred_u_ema.permute(2, 0, 1)
-        # 按概率降序排序
         sorted_probs, sorted_indices = torch.sort(pt, descending=True, dim=2)
         sorted_probs_ema, sorted_indices_ema = torch.sort(
             pt_ema, descending=True, dim=2
         )
 
-        # 计算累计概率，找到阈值类别
         accumulated_probs = torch.cumsum(sorted_probs, dim=2) #(frame, bs, 321)
         local_confidence = m*torch.mean(accumulated_probs[:,:,0]) + (1-m)*global_confidence
         C_star = (accumulated_probs >= local_confidence).int().argmax(dim=2, keepdim=True)
@@ -296,7 +267,6 @@ def calculate_cnr_factor(pred_u_self, pred_u_ema, global_confidence, m,max_class
         C_star_ema = (accumulated_probs_ema >= local_confidence).int().argmax(dim=2, keepdim=True)
         delta_j = sorted_probs.gather(2, C_star)
         delta_j_ema = sorted_probs_ema.gather(2, C_star_ema)
-        # 获取主导类别
         sorted_probs, sorted_indices = (
             sorted_probs[:, :, :max_class_num],
             sorted_indices[:, :, :max_class_num],
@@ -309,12 +279,10 @@ def calculate_cnr_factor(pred_u_self, pred_u_ema, global_confidence, m,max_class
         dominant_classes_ema = (
             sorted_indices_ema * (sorted_probs_ema >= delta_j_ema).int()
         )
-        # 创建主导类别掩码
         h_j = torch.zeros_like(pt)
         h_j.scatter_(2, dominant_classes, 1)
         h_j_ema = torch.zeros_like(pt_ema)
         h_j_ema.scatter_(2, dominant_classes_ema, 1)
-        # 只取主导类别的概率
         y_star = h_j * pt
         y_star_ema = h_j_ema * pt_ema
         dominant_prob_sum = torch.sum(y_star, dim=2) #(64,bs)
@@ -376,59 +344,41 @@ def contrastive_loss4(query1, predition1, query2, predition2, weights, linear_wi
     pred1 = torch.softmax(predition1,dim=1)
     pred2 = torch.softmax(predition2, dim=1)
 
-    # 扩展维度以适配批量计算
     query1 = query1.permute(0, 2, 1)  # (batch_size, query_num, 321)
     query2 = query2.permute(0, 2, 1)  # (batch_size, query_num, 321)
-
-    # 计算正样本相似度 一行是query1对于每一个query2的相似度
     pos_sim = F.cosine_similarity(query1.unsqueeze(2), query2.unsqueeze(1), dim=3)  # (batch_size, query_num, query_num)
 
     mask = torch.eye(query_num, device=pos_sim.device).unsqueeze(0).expand(batch_size, -1,
-                                                                           -1)  # (batch_size, query_num, query_num)
+                                                                           -1)  
     pos_sim_diag = pos_sim * mask  # (batch_size, query_num, query_num)
 
-    # 计算负样本相似度
-    neg_sim = pos_sim.clone() *(1-mask)  # 反转单位矩阵
+    neg_sim = pos_sim.clone() *(1-mask) 
 
-    #torch.fill_diagonal_(neg_sim, -float('inf'))  # 确保 i != j 的情况
+    #torch.fill_diagonal_(neg_sim, -float('inf')) 
 
-    # 计算正样本置信度和最大置信度
-    positive_confid = pred2.permute(0,2,1)  # (batch_size, query_num, 321)
-    tch_value, tch_index = torch.max(positive_confid, dim=2)  # (batch_size, query_num)
+    positive_confid = pred2.permute(0,2,1)  
+    tch_value, tch_index = torch.max(positive_confid, dim=2)  
     #print(tch_value)
 
-    # 计算学生置信度
-    anchor_confid = pred1.permute(0,2,1)  # (batch_size, query_num, 321)
-    # 创建索引
-    batch_indices = torch.arange(batch_size).unsqueeze(1)  # (batch_size, 1)
-    query_indices = torch.arange(query_num).unsqueeze(0)  # (1, query_num)
-
-    # 使用高级索引从 a 中提取值
-    stu_value = anchor_confid[batch_indices, query_indices, tch_index] # (batch_size, query_num)
+    anchor_confid = pred1.permute(0,2,1)  
+    batch_indices = torch.arange(batch_size).unsqueeze(1)  
+    query_indices = torch.arange(query_num).unsqueeze(0) 
+    stu_value = anchor_confid[batch_indices, query_indices, tch_index] 
 
     update_weight = torch.sum(tch_value) / (batch_size * query_num)
     #print(update_weight)
 
-    #对阈值全局调整
-
-    global_weight = m * weights + (1-m) * update_weight  # 这里应该是应用一个ema去更新参数 （321， dim）
+    global_weight = m * weights + (1-m) * update_weight  
     with torch.no_grad():
         output = global_weight.clone()
 
-    # 接下来对阈值局部调整
     l2_norms = torch.norm(linear_wieght, dim=1) #（321）
 
     l2_norms_max = torch.max(l2_norms)
 
-
-    # 每个元素除以L2范数的和
     normalized_l2_norms = l2_norms / l2_norms_max
     #print(torch.max(normalized_l2_norms,dim=0))
     global_weight = global_weight.cuda() * normalized_l2_norms.cuda()
-
-
-
-    # 计算有效正样本相似度和负样本相似度
     valid_mask = (stu_value <= tch_value) #& (tch_value >= threshold)  # (batch_size, query_num)
     consist_mask = torch.ones(batch_size, query_num)
 
@@ -436,24 +386,21 @@ def contrastive_loss4(query1, predition1, query2, predition2, weights, linear_wi
     list2=[]
     for bs in range(batch_size):
         list1 = []
-        q = tch_index[bs,:]   #每一个batch 教师的预测索引(64)
+        q = tch_index[bs,:]   
         qq = tch_value[bs, :]
         query_mask = (qq >= global_weight[q])
         valid_mask[bs] = valid_mask[bs] * query_mask
         consist_mask[bs] = query_mask
         for index in range(query_num):
-            v = q[index]  #每一个batch 每一个时间 帧教师的预测索引(1)
+            v = q[index]  
             label = (q != v).int()
             list1.append(label.unsqueeze(0))
         list2.append(torch.cat(list1, dim=0).unsqueeze(dim=0))
     mask2 = torch.cat(list2, dim=0)
 
     neg_sim = neg_sim * mask2
-    pos_sim = pos_sim_diag * valid_mask.unsqueeze(2).expand(-1, -1, query_num)  # 应用掩码
+    pos_sim = pos_sim_diag * valid_mask.unsqueeze(2).expand(-1, -1, query_num) 
 
-
-
-    # 计算损失
     pos_sim = pos_sim.sum(dim=2)  # (batch_size, query_num)
     neg_sim = neg_sim.sum(dim=2)  # (batch_size, query_num)
     loss = 0
@@ -476,7 +423,7 @@ def contrastive_loss4(query1, predition1, query2, predition2, weights, linear_wi
 
 
     #loss = -torch.log(pos_sim + 1e-2/ (pos_sim + neg_sim + 1e-2))  # (batch_size, query_num)
-    loss = loss / batch_size  # 平均损失
+    loss = loss / batch_size  
     with torch.no_grad():
         hold_num = torch.sum(consist_mask)
 
@@ -530,12 +477,8 @@ def calculate_contrastive_loss(
 ):
     batch_size = query1.shape[0]
     query_num = query1.shape[2]  # (batch_size, 321, query_num)
-
-    # 扩展维度以适配批量计算
     query1 = query1.permute(0, 2, 1)  # (batch_size, query_num, 321)
     query2 = query2.permute(0, 2, 1)  # (batch_size, query_num, 321)
-
-    # 计算正样本相似度 一行是query1对于每一个query2的相似度
     pos_sim = F.cosine_similarity(
         query1.unsqueeze(2), query2.unsqueeze(1), dim=3
     )  # (batch_size, query_num, query_num)
@@ -544,36 +487,26 @@ def calculate_contrastive_loss(
         torch.eye(query_num, device=pos_sim.device)
         .unsqueeze(0)
         .expand(batch_size, -1, -1)
-    )  # (batch_size, query_num, query_num)
+    ) 
     pos_sim_diag = pos_sim * mask  # (batch_size, query_num, query_num)
 
-    # 计算负样本相似度
-    neg_sim = pos_sim.clone() * (1 - mask)  # 反转单位矩阵
-
-    # torch.fill_diagonal_(neg_sim, -float('inf'))  # 确保 i != j 的情况
-
-    # 计算正样本置信度和最大置信度
+    neg_sim = pos_sim.clone() * (1 - mask)  
     positive_confid = pred2.permute(0, 2, 1)  # (batch_size, query_num, 321)
     tch_value, tch_index = torch.max(positive_confid, dim=2)  # (batch_size, query_num)
 
-    # 计算学生置信度
     anchor_confid = pred1.permute(0, 2, 1)  # (batch_size, query_num, 321)
-    # 创建索引
     batch_indices = torch.arange(batch_size).unsqueeze(1)  # (batch_size, 1)
     query_indices = torch.arange(query_num).unsqueeze(0)  # (1, query_num)
 
-    # 使用高级索引从 a 中提取值
     stu_value = anchor_confid[
         batch_indices, query_indices, tch_index
-    ]  # (batch_size, query_num)
-
-    # 计算有效正样本相似度和负样本相似度
+    ]  
     valid_mask = (stu_value <= tch_value) & (
         tch_value >= threshold
     )  # (batch_size, query_num)
     pos_sim = pos_sim_diag * valid_mask.unsqueeze(2).expand(
         -1, -1, query_num
-    )  # 应用掩码
+    ) 
 
     list2 = []
     for bs in range(batch_size):
@@ -588,7 +521,6 @@ def calculate_contrastive_loss(
 
     neg_sim = neg_sim * mask2
 
-    # 计算损失
     pos_sim = pos_sim.sum(dim=2)  # (batch_size, query_num)
     neg_sim = neg_sim.sum(dim=2)  # (batch_size, query_num)
     loss = 0
@@ -606,8 +538,7 @@ def calculate_contrastive_loss(
         else:
             continue
 
-    # loss = -torch.log(pos_sim + 1e-2/ (pos_sim + neg_sim + 1e-2))  # (batch_size, query_num)
-    loss = loss / batch_size  # 平均损失
+    loss = loss / batch_size 
 
     return loss
 if __name__ == '__main__':
